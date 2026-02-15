b6bf97782f15b5559c371a11a3f320b705ada837
Alexa Saur <a2saur@autoreg-6811147.dyn.wpi.edu>
2026-02-15 15:04:22 -0500

    Fixed errors caused by merge

--- app/user/user_routes.py
+++ app/user/user_routes.py
@@ -5,9 +5,10 @@ from flask import render_template, flash, redirect, url_for, request, jsonify
 from flask_login import current_user, login_required
 import sqlalchemy as sqla
 from app import db
-from app.main.models import User
+from app.main.models import User, Ingredient, UserIngredientListUse, UserGroceryListUse
 
 from app.user.user_forms import EditForm
+from app.recipe.recipe_forms import IngredientSubmitForm
 
 from app.user import user_blueprint as bp_user
 
@@ -56,6 +57,73 @@ def remove_saved_recipe(recipe_id):
     return
 
 @bp_user.route('/user/ingredients', methods=['GET','POST'])
-@login_required
+# @login_required
 def view_ingredients():
-    return render_template('profile.html', title="User Profile", user=current_user)
+    iform = IngredientSubmitForm(prefix="curr")
+    gform = IngredientSubmitForm(prefix="groc")
+
+    # get the user's current ingredients 
+    curr_ingredients = current_user.get_curr_ingredients()
+    # get the user's grocery list ingredients
+    grocery_list = current_user.get_grocery_list()
+    
+    if iform.submit.data and iform.validate():
+        ingredient = db.session.scalars(sqla.select(Ingredient).where(Ingredient.name == iform.ingredientName.data)).first()
+        if not ingredient:
+            ingredient = Ingredient(name=iform.ingredientName.data)
+            db.session.add(ingredient)
+            db.session.commit()
+        current_user.add_ingredient(ingredient, iform.quantity.data, iform.unit.data)
+        return redirect(url_for('user.view_ingredients'))
+    
+    elif gform.submit.data and gform.validate():
+        ingredient = db.session.scalars(sqla.select(Ingredient).where(Ingredient.name == gform.ingredientName.data)).first()
+        if not ingredient:
+            ingredient = Ingredient(name=gform.ingredientName.data)
+            db.session.add(ingredient)
+            db.session.commit()
+        current_user.add_grocery(ingredient, gform.quantity.data, gform.unit.data)
+        return redirect(url_for('user.view_ingredients'))
+    return render_template('view_ingredients.html', title="Ingredients", ingredients=curr_ingredients, grocery_list=grocery_list, iform=iform, gform=gform)
+
+@bp_user.route('/user/move_or_delete_grocery', methods=['POST'])
+# @login_required
+def move_or_delete_grocery():
+    action = request.form.get('action')
+    selected_grocery_ids = request.form.getlist('grocery_ids')
+
+    if not selected_grocery_ids:
+        flash('No ingredients selected to move.')
+        return redirect(url_for('user.view_ingredients'))
+    
+    for ing_id in selected_grocery_ids:
+        grocery = db.session.scalars(sqla.select(UserGroceryListUse).where(UserGroceryListUse.user_id==current_user.id).where(UserGroceryListUse.ingredient_id==int(ing_id))).first()
+
+        if action == "purchased":
+            ingredient = db.session.get(Ingredient, int(ing_id))
+            current_user.add_ingredient(ingredient, grocery.amount, grocery.unit)
+            db.session.delete(grocery)
+        elif action == "delete":
+            db.session.delete(grocery)
+    
+    db.session.commit()
+    if action == "move":
+        flash("Selected ingredients moved to current ingredients.")
+    elif action == "delete":
+        flash("Selected grocery items deleted.")
+    return redirect(url_for('user.view_ingredients'))
+
+@bp_user.route('/user/delete_ingredient', methods=['POST'])
+# @login_required
+def delete_ingredient():
+    selected_ingredient_ids = request.form.getlist('ingredient_ids')
+    if not selected_ingredient_ids:
+        flash('No ingredients selected to delete.')
+        return redirect(url_for('user.view_ingredients'))
+    
+    for ing_id in selected_ingredient_ids:
+        ingredient = db.session.scalars(sqla.select(UserIngredientListUse).where(UserIngredientListUse.user_id==current_user.id).where(UserIngredientListUse.ingredient_id==int(ing_id))).first()
+        db.session.delete(ingredient)
+    db.session.commit()
+    flash("Selected ingredients deleted.")
+    return redirect(url_for('user.view_ingredients'))
