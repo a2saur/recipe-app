d38e96380babb0f71ed99ccfabf3a2d18517729f
sunnykang813 <sunnykang813@gmail.com>
2026-02-20 15:21:05 -0500

    Implemented recipe recommendation

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,6 +6,7 @@ from app import db
 from app.main.models import Recipe, RecipeIngredientUse, Ingredient, Tag, User, recipe_tags_table, UserGroceryListUse, UserIngredientListUse
 from app.main.forms import EmptyForm, SortForm
 from app.auth.auth_forms import RegistrationForm
+from app.main.helpers import get_recommended_recipes
 
 from app.main import main_blueprint as bp_main
 
@@ -16,6 +17,11 @@ from app.main import main_blueprint as bp_main
 def index(sort_data="Date"):
     empty_form = EmptyForm()
     sort_form = SortForm()
+
+    rec_recipes = get_recommended_recipes(current_user.id)
+
+
+    # sort by implementation
     base_query = sqla.select(Recipe).where(Recipe.is_draft == False)
     if request.method == 'POST':
         sort_data = sort_form.sortby.data
@@ -26,7 +32,13 @@ def index(sort_data="Date"):
                 recipes = base_query.join(Recipe.writer).order_by(User.is_certified.desc())
             else:
                 recipes = base_query.order_by(Recipe.timestamp.desc())
+
+    # default: order by most recent 
     if request.method == 'GET':
             recipes = base_query.order_by(Recipe.timestamp.desc())
     all_recipes  = db.session.scalars(recipes).all() 
-    return render_template('index.html', title="", recipes=all_recipes, form=empty_form, sortform = sort_form)
+    recipe_len = len(all_recipes)
+
+
+
+    return render_template('index.html', title="", recipe_len=recipe_len, rec_recipes=rec_recipes, recipes=all_recipes, form=empty_form, sortform = sort_form)
